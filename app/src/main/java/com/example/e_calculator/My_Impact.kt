33ec4c19bd186b771ba264f7067af5a2f3222322
gotech123 <117999070+gotech123@users.noreply.github.com>
2024-02-27 13:59:23 +0530

    newpage

---
+++ app/src/main/java/com/example/e_calculator/My_Impact.kt
@@ -0,0 +1,11 @@
+package com.example.e_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class My_Impact : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_impact)
+    }
+}
