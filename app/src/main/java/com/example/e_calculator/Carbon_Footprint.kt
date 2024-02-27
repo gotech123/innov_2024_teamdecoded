dd455092e6c728ecb791d8644aa78c9ff78fc7d0
gotech123 <117999070+gotech123@users.noreply.github.com>
2024-02-27 12:47:48 +0530

    CarbonCreate

---
+++ app/src/main/java/com/example/e_calculator/Carbon_Footprint.kt
@@ -0,0 +1,11 @@
+package com.example.e_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Carbon_Footprint : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_carbon_footprint)
+    }
+}
