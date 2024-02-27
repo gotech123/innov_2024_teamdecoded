274d6038c39485576eeea68c07f13041c110e4b2
gotech123 <117999070+gotech123@users.noreply.github.com>
2024-02-27 11:59:30 +0530

    LOGIN

--- app/src/main/java/com/example/e_calculator/Login_Page.kt
+++ app/src/main/java/com/example/e_calculator/Login_Page.kt
@@ -3,9 +3,9 @@ package com.example.e_calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Login_Page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login_page)
     }
 }
